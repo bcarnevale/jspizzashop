c0f4634145c09b6714565285721135e18560a775
bcarnevale <bianca.carnevale@hotmail.com>
2018-11-15 15:07:42 +1100

    Input title with a random colour generator

--- js/index.js
+++ js/index.js
@@ -1,11 +1,40 @@
 const title = document.querySelector('#title')
+const getButton = document.querySelector('#getPizza')
+const input = document.querySelector("#inputBox")
+// console.dir(getButton)
 
-function changeInputBoxText() {
-    title.innerText=(inputBox.value)
+// function changeInputBoxText() {
+//     title.innerText=(inputBox.value)
+// }
+
+input.addEventListener('input', changeText)
+
+function getRandomColor() {
+    let color =  "#" + (Math.random() * 0xFFFFFF << 0).toString(16);
+    return color;
 }
 
-const getButton = document.querySelector('#getPizza')
+function changeText(e) {
+    const text = e.target.value
+    title.innerHTML = ''
+    text.split('')
+        .map(function(c) {
+            return addColorToChar(c)
+        })
+        .forEach(function(elm) {
+            title.appendChild(elm)
+        })
+}
 
-getButton.addEventListener('click', function() {
-    alert("Getting you your pizza!!")
-})
+function addColorToChar(char) {
+    const span = document.createElement('span')
+    span.innerText = char
+    span.style.color = getRandomColor()
+    return span
+}
+
+// getButton.addEventListener('click', function(e) {
+//     e.target.style.backgroundColor = getRandomColor()
+//     e.target.classList.add('getpizza')
+//     alert("Getting you your pizza!!")
+// })
